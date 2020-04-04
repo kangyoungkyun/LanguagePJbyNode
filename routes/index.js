755bc33dba60bdf481bfe0efd30b261be80cfb53
k <abcnt@naver.com>
2020-04-04 22:11:24 +0900

    비디오 화면 셋팅, 정적파일 셋팅 및 경로 설정, 화면 이동 셋팅

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 홈 */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+/* 리스트 페이지로 이동 */
+router.get('/showList', function(req, res, next) {
+  res.render('list', { title: 'list' });
+});
+
+
+/* 비디오 재생 페이지로 이동 */
+router.get('/runVideo', function(req, res, next) {
+  res.render('video', { title: 'runVideo' });
 });
 
 module.exports = router;
